0c64e713d68647a4a1f06ff6bb5947461f2d798e
Hayato.Otsuka <hayato.otsuka@ibm.com>
2023-05-24 10:55:54 +0900

    Stash

---
+++ src/app/nsclient.ts
@@ -0,0 +1,93 @@
+import axios, { AxiosInstance } from 'axios';
+
+export interface SeekUserSession {
+    metadata: { user_id: string };
+    system: { session_id: string };
+}
+
+// Seek options type
+export interface SeekOptions {
+    personalize?: {
+        preferredName: string;
+        noWelcome: string;
+        products: string[];
+        additionalDetails: string;
+    };
+    language?: string;
+    filter?: string;
+    company?: string;
+    semanticScore?: { enabled: string; primary: string };
+    lastTurn?: { input: string; response: string };
+    pullToCompany?: string;
+    promptEngineering?: string;
+    promptEngineeringPhrase?: string;
+    answerEngineering?: string;
+    warnConfidence?: string;
+    warnConfidenceText?: string;
+    minConfidence?: string;
+    minConfidenceText?: string;
+    misTolerance?: string;
+    sensitiveText?: string;
+    minText?: string;
+    maxWords?: string;
+    url?: string;
+    stump?: string;
+    includeSourceResults?: string;
+}
+
+// Seek request and response types
+export interface SeekRequest {
+    question: string;
+    context?: object;
+    user_session?: SeekUserSession;
+    options?: SeekOptions;
+}
+
+export interface SeekResponsePassages {
+    passage: string;
+    id: string;
+    score: number;
+    url: string;
+    document: string;
+    train: string;
+    highlights: { d: string; position: number; length: number }[];
+}
+
+export interface SeekResponse {
+    answer: string;
+    score: string;
+    url: string;
+    document: string;
+    time: number;
+    kbTime: number;
+    passages?: SeekResponsePassages[];
+    KBscore: number;
+    kbCoverage: number;
+    sentiment: number;
+    semanticScore: number;
+    totalCount: number;
+}
+
+const createClient = (apiKey: string, instanceId: string): AxiosInstance => {
+    return axios.create({
+        baseURL: `https://api.neuralseek.com/v1/${instanceId}`,
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': apiKey,
+        },
+        responseType: 'json',
+    });
+}
+
+export class NSClient {
+    private client: AxiosInstance;
+
+    constructor(apiKey: string, instanceId: string) {
+        this.client = createClient(apiKey, instanceId);
+    }
+
+    public async seek(req: SeekRequest): Promise<SeekResponse> {
+        const response = await this.client.post<SeekResponse>('/seek', req);
+        return response.data as Promise<SeekResponse>;
+    }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,133 @@
-import Image from 'next/image'
+"use client"
+
 import styles from './page.module.css'
+import React, {useState} from "react";
+import {Theme} from '@carbon/react'
+import {NSClient, SeekResponse} from "@/app/nsclient";
+import { Header } from "carbon-components-react";
+import { ColorRing } from "react-loader-spinner";
+
+import { Table, Accordion } from 'react-bootstrap';
+import { FaLightbulb } from "react-icons/fa"
+import { IconContext } from "react-icons"
 
 export default function Home() {
+  const [inputText, setInputText] = useState<string>('')
+  const [result, setResult] = useState<SeekResponse|null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const onChangeSearchText = (e: {target: HTMLInputElement, type: string}): void => setInputText(e.target.value);
+
+  const nsClient = new NSClient(process.env.NEXT_PUBLIC_NS_APIKEY, process.env.NEXT_PUBLIC_NS_INSTANCE_ID);
+
+  const sendRequest = async () => {
+    setLoading(true)
+    const response = await nsClient.seek({
+      question: inputText,
+      context: {},
+      user_session: {
+        metadata: {
+          user_id: 'test_user_id',
+        },
+        system: {
+          session_id: 'test_session_id'
+        },
+      },
+      options: {
+        language: "ja",
+        includeSourceResults: 'true',
+      }
+    })
+
+    setResult(response)
+    setLoading(false)
+
+    console.log(response)
+  }
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Theme theme={'g10'} className={styles.theme}>
+      <ColorRing
+          visible={loading}
+          width={'25%'}
+          height={'25%'}
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+          wrapperStyle={{position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)'}}
+      />
+      <Header aria-label="NeuralseekDemo" className={styles.header}>
+        <a href="#">NeuralSeek Demo</a>
+      </Header>
+      <div className={styles.main}>
+        <div className={styles.searchContent}>
+          <input type={'text'} onChange={onChangeSearchText} placeholder={'検索したい語句を入力してください'} className={styles.searchInputText}/>
+          <button onClick={sendRequest} className={styles.searchButton}>検索</button>
         </div>
+        {
+          result === null ? <></> : (
+            <div className={styles.resultContent}>
+              <div className={styles.answer}>
+                <IconContext.Provider value={{ color: '#a3a3a3'}}>
+                  <FaLightbulb />
+                </IconContext.Provider>
+                {result.answer}
+              </div>
+              <Table striped={true} bordered={true} responsive={true} className={styles.answerTable}>
+                <tbody>
+                  <tr>
+                    <td>見つかったDocument</td>
+                    <td>{result.totalCount} 件</td>
+                  </tr>
+                  <tr>
+                    <td>解答自信度</td>
+                    <td>{result.score} %</td>
+                  </tr>
+                  <tr>
+                    <td>セマンティックマッチ度</td>
+                    <td>{result.semanticScore}</td>
+                  </tr>
+                  <tr>
+                    <td>ドキュメントベースに対する自信度</td>
+                    <td>{result.KBscore}</td>
+                  </tr>
+                  <tr>
+                    <td>ドキュメントからの引用率</td>
+                    <td>{result.kbCoverage}</td>
+                  </tr>
+                  <tr>
+                    <td>ドキュメントからのレスポンスタイム</td>
+                    <td>{result.kbTime}</td>
+                  </tr>
+                  <tr>
+                    <td>トータルレスポンスタイム</td>
+                    <td>{result.time}</td>
+                  </tr>
+                  {
+                    result.passages ? result.passages.map((p, i) => {
+                      return (
+                        <tr key={i}>
+                          <td colSpan={2}>
+                            <Accordion>
+                              <Accordion.Item eventKey={i.toString()}>
+                                <Accordion.Header>
+                                  <span className={styles.passageAccordionHeaderDocument}> {p.document} </span>
+                                  <span className={styles.passageAccordionHeaderScore}> {p.score} </span>
+                                </Accordion.Header>
+                                <Accordion.Body>
+                                  <div className={styles.passageAccordionBody} dangerouslySetInnerHTML={{__html: p.passage}}></div>
+                                </Accordion.Body>
+                              </Accordion.Item>
+                            </Accordion>
+                          </td>
+                        </tr>
+                      )
+                    }) : <></>
+                  }
+                </tbody>
+              </Table>
+            </div>
+          )
+        }
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </Theme>
   )
 }
